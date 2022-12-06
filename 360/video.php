92862affd5f99e9ca3f60b4929d56e89ab2f40a3
Adílerson Casa Grande <89746040+adilerson@users.noreply.github.com>
2022-12-06 14:53:03 -0300

    v 1.07

--- 360/video.php
+++ 360/video.php
@@ -8,7 +8,6 @@
     <script type="text/javascript" src="js/qrcode.js?teste=1"></script>
     <link href="https://getbootstrap.com/docs/5.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <title>CG-360</title>
-    
 </head>
 <body>    
 <?php
