a1d108e085ca5f41ad17152a1d29255bd5d5d87b
Adílerson Casa Grande <89746040+adilerson@users.noreply.github.com>
2022-12-26 18:21:22 -0300

    cssFront

---
+++ 360/startCell.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/png" href="/360Linux/360/img/favicon.ico">
+    <script type="text/javascript" src="js/jquery-2.2.4.min.js"></script>
+    <link href="css/style.css?teste=04" rel="stylesheet">
+    <title>StartCell 360</title>
+</head>
+
+<body>
+    <div class="flex collum hCenter">
+        <div class="btn">Ligar</div>
+    </div>
+    <div id="status"></div>
+
+</body>
+<script>
+    $(".btn").click(function(){
+        $("#status").load("https://192.168.36.36:3000/comando?status=1")
+        //$("#status").load("startCellScript.php");
+    });
+</script>
+<style>
+html{
+display: flex;
+    justify-content: center;
+    align-items: center;
+    align-content: center;
+    height: 80vh;
+}
+.btn{
+    font-size: 3rem;
+    font-family: 'Roboto';
+    text-transform: uppercase;
+    font-weight: bold;
+}
+</style>
--- futuroIndex.php
+++ futuroIndex.php
@@ -18,11 +18,13 @@
     <div class="flex hCenter vCenter coluna text-white m-2" style="flex-direction: column;">
         <h1>Celular</h1>
         <div class="fs-1 p-1">
-            <a href="https://<?php echo $ip; ?>:3000" target="_blank" rel="noopener noreferrer" class="btn">Passo 1</a>
+            <a href="https://<?php echo $ip; ?>:3000" target="_blank" rel="noopener noreferrer" class="btn">Acessar</a>
         </div>
+        <!--        
         <div class="fs-1 p-1">
             <a href="https://<?php echo $ip; ?>/360Linux/front" target="_blank" rel="noopener noreferrer" class="btn">Passo 2</a>
         </div>
+        -->
 
         <h1>Aparelho Start</h1>
         <div class="fs-1 p-1">
@@ -31,10 +33,12 @@
 
         <div class="divisao"></div>
 
-        <h1>Admin</h1>
+        <h1>Tablet</h1>
         <div class="fs-1 p-1">
             <a href="http://<?php echo $ip; ?>/360Linux/360/" target="_blank" rel="noopener noreferrer" class="btn">Eventos</a>
         </div>
+
+        <h1>Administrador</h1>
 
         <div class="fs-1 p-1">
             <a href="http://<?php echo $ip; ?>/360Linux/360/config.php" target="_blank" rel="noopener noreferrer" class="btn">Configurações</a>
