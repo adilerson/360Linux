fbd81fbe7ac3154f63cd88b185958a5672abfe9b
Adílerson Casa Grande <89746040+adilerson@users.noreply.github.com>
2022-12-29 12:35:33 -0300

    Update app.service.ts

--- videoSpinAPI/src/app.service.ts
+++ videoSpinAPI/src/app.service.ts
@@ -3,6 +3,8 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return '<link href="https://192.168.36.36/360Linux/360/css/style.css" rel="stylesheet"><h1 style="display:flex; flex-direction: column; padding-top: 10rem; justify-content: center; font-size:2.5rem;width: 100%;text-align: center;"><a href="https://192.168.36.36/360Linux/front" class="btn p-3" style="font-size: 2.5rem">Acessar Eventos</a></h1>';
+    return `<style>html, body{background-color:#373e46 !important;}</style><h1 style="display: flex;flex-direction: column;justify-content: center;font-size: 2.5rem;width: 100%;height: 80vh;text-align: center;"><a href="https://192.168.36.36/360Linux/front" style="font-family: system-ui;background-color: #56baed;border: none;color: white;text-align: center;text-decoration: none;display: inline-block;font-size: 3rem;-webkit-box-shadow: 0 10px 30px 0 rgb(95 186 233 / 40%);box-shadow: 0 10px 30px 0 rgb(95 186 233 / 40%);-webkit-border-radius: 5px 5px 5px 5px;border-radius: 5px 5px 5px 5px;margin: 15px 20px 14px 20px;cursor: pointer;padding: 3rem;" style="font-size: 2.5rem">Acessar Eventos</a></h1>`;
   }
 }
+
+
