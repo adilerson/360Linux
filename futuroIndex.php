77fc981fe1982c7a17bad8dfde93d2fbbef71e3e
Adílerson Casa Grande <89746040+adilerson@users.noreply.github.com>
2023-01-02 20:00:37 -0300

    Update futuroIndex.php

--- futuroIndex.php
+++ futuroIndex.php
@@ -4,7 +4,7 @@
     $escaped_url = file_get_contents($url);
 
     function dados($str){
-        $json = file_get_contents("360Linux/360/data.json");
+        $json = file_get_contents("/var/www/html/360Linux/360/data.json");
         $data = json_decode($json);
 
 
